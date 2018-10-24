71c029395ac4cc66f80e353db3fa5e0352ef148e
student <student@itlab.pmmk.pte.hu>
2018-10-24 08:53:34 +0200

    Singing

---
+++ SongCompetition/Competitor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongCompetition
+{
+    class Competitor
+    {
+        //fields
+        private int idNumber;
+        private string name;
+        private string department;
+
+        private int score;
+
+        //defining a unique number
+        private static int itemsNumber;
+
+        //constructor
+        public Competitor(string name,string department)
+        {
+            this.name = name;
+            this.department = department;
+
+            //unique number 
+            itemsNumber++;
+            idNumber = itemsNumber;
+        }
+        //methods
+        public void Scoring(int point)
+        {
+            score += point;
+        }
+        public override string ToString()
+        {
+            return idNumber + "\t" + name + "\t" + department + "\t" + score + "points";
+        }
+        //properties
+        public int IdNumber
+        {
+            get { return idNumber; }
+        }
+        public string Name
+        {
+            get { return name; }
+        }
+        public string Department
+        {
+            get { return department; }
+        }
+        public int Score
+        {
+            get { return score; }
+        }
+    }
+}
---
+++ SongCompetition/Controller.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SongCompetition
+{
+    class Controller
+    {
+        private List<Competitor> Competitors = new List<Competitor>();
+        private int juryMember = 5;
+        private int scoreLimit = 10;
+            
+       public void Start()
+        {
+            InputData();
+            DisplayData("\nParticipants: \n);
+            Competition();
+            DisplayData("\nResults: \n");
+            Results();
+            Search();
+        }
+
+        private void InputData()
+        {
+            String name, department;
+            Competitor competitor;
+
+            //read the data
+            StreamReader streamreader = new StreamReader("competitor.txt");
+            while (!streamreader.EndOfStream)
+            {
+                name = streamreader.ReadLine();
+                department = streamreader.ReadLine();
+                competitor = new Competitor(name, department);
+                //add the new object to the list
+                competitors.Add(competitor);
+            }
+        }
+
+        private object Competition()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DisplayData(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Search()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
